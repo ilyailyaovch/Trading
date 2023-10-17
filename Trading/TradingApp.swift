ae4bbbe2aeedb863343cd55c161dd49bc03b4274
Ilya Ovchinnikov <jewovch@gmail.com>
2023-10-17 18:27:21 +0300

    Initial Commit

---
+++ Trading/TradingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingApp.swift
+//  Trading
+//
+//  Created by Ilya Ovchinnikov on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TradingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
